02cd3758f15e4341962e05260173389e5f1bc98a
ted.kang <ted.kang@kakaocorp.com>
2021-07-21 01:26:12 +0900

    [release] ver.0.0.2-SNAPSHOT VelocityLayoutViewResolver는 EmbeddedTomcat에서 toolbox.xml을 찾지 못함 EmbeddedVelocityToolboxView를 활용한 커스텀 LayoutView와 ViewResolver가 필요하다. cf) https://github.com/spring-projects/spring-boot/issues/4211 작업 내용 : 1. EmbeddedVelocityLayoutView 작성 2. EmbeddedVelocityLayoutViewResolver 작성 3. PathBasedVelocityLayoutViewResolver 가 EmbeddedVelocityLayoutViewResolver 를 상속하도록 변경

--- src/main/java/org/enemfk777/springboot2velocity/velocity/PathBasedVelocityLayoutViewResolver.java
+++ src/main/java/org/enemfk777/springboot2velocity/velocity/PathBasedVelocityLayoutViewResolver.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
-public class PathBasedVelocityLayoutViewResolver extends VelocityLayoutViewResolver {
+public class PathBasedVelocityLayoutViewResolver extends EmbeddedVelocityLayoutViewResolver {
 
   private static final String PATH_SEPARATOR = "/";
 
